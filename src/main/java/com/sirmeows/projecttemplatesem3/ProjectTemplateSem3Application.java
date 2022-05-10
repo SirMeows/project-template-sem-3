ea2216b57635d1be95ec6ae52e05cdc0f7923b6b
SirMeows <araelbrighteyes@gmail.com>
2022-05-10 14:59:28 +0200

    initial commit, folder structure, mvn dependencies etc.

---
+++ src/main/java/com/sirmeows/projecttemplatesem3/ProjectTemplateSem3Application.java
@@ -0,0 +1,13 @@
+package com.sirmeows.projecttemplatesem3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTemplateSem3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTemplateSem3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sirmeows/projecttemplatesem3/ProjectTemplateSem3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirmeows.projecttemplatesem3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTemplateSem3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
